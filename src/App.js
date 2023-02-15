6ae26b368b9d551878c77d27acdce2f224ebfad1
Jonathan Amaral <jonathansamael@live.com>
2023-02-15 16:32:30 -0300

    "🚧 first commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    Teste...
     </div>
   );
 }
 
 export default App;
+
+
+{/* <a target="_blank" href="https://icons8.com/icon/63311/pokebola">Pokebola</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
